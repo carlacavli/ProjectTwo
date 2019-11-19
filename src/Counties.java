fabb47f33712c3004683fd74c0f31b51ec3eba9c
muberracavli <muberracav@hotmail.com>
2019-11-18 21:39:49 -0500

    Projecttwo

--- src/Counties.java
+++ src/Counties.java
@@ -11,16 +11,25 @@ public class Counties {
      * method should take a tree number(int) and return smallest number(int)
      * Write a Java method to find the smallest number among three numbers.
      */
+   public int smallestNumber(int a, int b,int c){
+          return Math.min(Math.min(a, b), c);
 
-
+ }
     /** Create a method
      * method name is middleCharacter    <-------   METHOD NAME SHOULD BE AS THIS
      * this method should take a String as parameter and return char
      * Write a Java method to display the middle character of a string
      * if the string length is even number return 0
      */
-
-
+    public char middleCharater (String a) {
+        char [] arr = a.toCharArray();
+        char x = arr[arr.length / 2];
+        if (a.length() % 2 == 0) {
+            return '0';
+        }
+        else
+            return x;
+    }
     /** Create a method
      * method name is dayOfTheWeek    <-------   METHOD NAME SHOULD BE AS THIS
      * this method return type is string parameter is number(int)
@@ -28,7 +37,30 @@ public class Counties {
      * if the number is 2 return tuesday
      * if the number bigger then 7 then return -->> this is not a expected input
      */
+    public String dayoftheWeek (int a) {
 
+        if (a == 1) {
+            return "Monday";
+        } else if (a == 2) {
+            return "Tuesday";
+        } else if (a == 3) {
+            return "Wednesday";
+        } else if (a == 4) {
+            return "Thursday";
+        } else if (a == 5) {
+            return "Friday";
+        } else if (a == 6) {
+            return "Saturday";
+        } else if (a == 7) {
+            return "Sunday";
+        } else if (a > 7) {
+
+
+        }
+        return "this is not a expected input";
+
+
+    }
 
     /**
      * if CountThis String in the myCounties ArrayList then return how many of CountThis string in the myCounties
@@ -39,10 +71,19 @@ public class Counties {
         int result = 0;
         // START THE CODE FROM HERE
 
+        if (!myCountries.contains(CountThis)) {
+            return -1;
+        }
 
-        // CODE END HERE
+        for (String a : myCountries) {
+            if (a.equals(CountThis)) {
+                result++;
+            }
+        }
         return result;
     }
+
+
 
     /**
      * sort the arrayList
@@ -51,6 +92,7 @@ public class Counties {
      */
     public ArrayList<String> sortArrayList(ArrayList<String> myCountries) {
 
+        Collections.sort(myCountries);
 
         return myCountries;
     }
@@ -63,7 +105,10 @@ public class Counties {
     public ArrayList<String> reverseTheList(ArrayList<String> myCountries) {
         ArrayList<String> reverse = new ArrayList<>();
 //       start writing the code here
-
+        Collections.reverse(myCountries);
+        for (String x: myCountries) {
+            reverse.add(x);
+        }
 
 //        ends here
         return reverse;
@@ -81,10 +126,21 @@ public class Counties {
      * return false
      */
     public boolean isFirstArrayListContainsSecondArrayList(ArrayList<String> firstArray, ArrayList<String> SecondAray) {
+        boolean a = false;
 
-
-        return false;
+        for (String x: firstArray) {
+            for (String y: SecondAray) {
+                if (x == y) {
+                    a = true;
+                }
+                else
+                    a = false;
+            }
+        }
+        return a;
     }
+
+
 
     /**
      * if ArrayList myCountries have a firstCountry then replace it with a secondCountry
@@ -94,9 +150,15 @@ public class Counties {
      * result should be Brazil Canada Mexico Canada
      */
     public ArrayList<String> replaceTheCountry(ArrayList<String> myCountries, String firstCountry, String SecondCounry) {
-
-
+        for (String x: myCountries) {
+            if (x.contains(firstCountry)) {
+                int a = myCountries.indexOf(firstCountry);
+                myCountries.set(a,SecondCounry);
+            }
+        }
         return myCountries;
     }
 
-}
+        }
+
+
